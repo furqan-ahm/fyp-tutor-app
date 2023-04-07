76f151a66c7a6a07b4beff3e821c62bce6d027ff
FurqanAhmed <furqanshaheer@gmail.com>
2023-04-07 22:17:52 +0500

    added user role enum and some colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+const baseColor = Color(0xFFF5F5F5);
+const primaryColor = Colors.blue;
+const accentColor = Colors.deepPurpleAccent;
+const bodyTextColor = Color(0xFF1E1C1C);
+const headingColor = Color(0xFFFFFFFF);
+
+const onBoardDotColor = Color(0xFFDBE6F0);
+const greyButtonColor = Color(0xFFEFEFEF);
+
+const isBlack = true;
+
+//font faimly: Poppins
+
+//font sizes: 17,  22,  25.5
+
+// mockup screen size: (width and height) 428x926 
+
+//onboard animated image size: 320 x 283
+
+// on board button size: 75x75
+// on board dot btns (combined) size: 75x9
+
+//heading fontWeight: 600
+//body fontWeight: 500
+
+// SplashScreen
+// SplashScreen Logo size: 302x267
