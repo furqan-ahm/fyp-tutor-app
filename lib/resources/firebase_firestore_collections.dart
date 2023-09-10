5751ffc7276ca7aad4ab109f5881ce4410a313b6
FurqanAhmed <furqanshaheer@gmail.com>
2023-09-10 20:34:42 +0500

    forgot to commit these resources

---
+++ lib/resources/firebase_firestore_collections.dart
@@ -0,0 +1,8 @@
+
+class FirebaseCollections {
+  static const String usersCollection = 'users';
+  static const String tutorshipCollection = 'tutorships';
+  static const String customComponentCollection = 'custom_components';
+  static const String ordersCollection = 'orders';
+  
+}
---
+++ lib/utils/enums.dart
@@ -0,0 +1,7 @@
+enum ProvidedService { Wash_Iron, Dry_Cleaning, Wash, Iron}
+
+
+enum TimeMode{AM, PM}
+
+
+enum PaymentMethod{Card, JazzCash, EasyPaisa, Cash_On_Delivery}
